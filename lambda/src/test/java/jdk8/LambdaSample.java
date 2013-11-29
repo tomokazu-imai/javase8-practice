e162c31da3f582870391c547a1cf4710cc00d8e3
tomokazu-imai <tomokazu.imai@ctc-g.co.jp>
2013-11-29 14:39:32 +0900

    add new test case

--- lambda/src/test/java/jdk8/LambdaSample.java
+++ lambda/src/test/java/jdk8/LambdaSample.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -27,6 +28,22 @@ public class LambdaSample {
 
         assertThat(actual.get(0), is("HELLO WORLD"));
         assertThat(actual.get(1), is("HOGE"));
+    }
+
+    @Test
+    public void 先頭文字がhだった場合に全て大文字にして最初の検索だけ返す() {
+        List<String> strList = new ArrayList<>();
+        strList.add("hello world");
+        strList.add("foo");
+        strList.add("bar");
+        strList.add("hoge");
+
+        Optional<String> actual = strList.stream()
+                .filter(s -> s.startsWith("h"))
+                .map(String::toUpperCase)
+                .findFirst();
+
+        assertThat(actual.get(), is("HELLO WORLD"));
     }
 
     @Test
